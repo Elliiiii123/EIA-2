465f2ef4188e9534e89cd221c3b7a90814b57958
Elliiiii123 <elitorolanger@gmail.com>
2024-04-18 21:41:21 +0200

    Einkaufsliste

--- A_03/Einkausliste.ts
+++ A_03/Einkausliste.ts
@@ -4,7 +4,9 @@ namespace Einkaufsliste{
     function handleLoad(_event: Event): void {
         console.log("Start");
         let form: HTMLDivElement = <HTMLDivElement>document.querySelector("div#form");
+        let deleteButton: HTMLButtonElement = <HTMLButtonElement>document.querySelector("div#delete");
 
+        deleteButton.addEventListener("click", DeleteButton);
         form.addEventListener("change", handleChange);
     }
 
@@ -15,6 +17,16 @@ namespace Einkaufsliste{
         console.log(input.value);
     }
 
+    function DeleteButton(_event: MouseEvent): void {
+        console.log("Das Div 'Einträge' wurde gelöscht.");
+        const eintraegeDiv: HTMLDivElement = <HTMLDivElement>document.querySelector("div#Einträge");
+        if (eintraegeDiv) {
+            eintraegeDiv.remove();
+        } else {
+            console.log("Einträge Div nicht gefunden.");
+        }
+    }
+        
 
     // function addFields() {
     // }
