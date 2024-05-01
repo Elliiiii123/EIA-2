07f26abcf6f4e1e1b8542e17c39a48643b6d1a67
Elliiiii123 <elitorolanger@gmail.com>
2024-05-01 17:58:58 +0200

    Klaklcnsalicf

---
+++ A_08/generativeKunst.js
@@ -0,0 +1,112 @@
+"use strict";
+var GenerativeKunst;
+(function (GenerativeKunst) {
+    window.addEventListener("load", handleLoad);
+    let crc2;
+    function handleLoad(_event) {
+        let canvas = document.querySelector("canvas");
+        if (!canvas)
+            return;
+        crc2 = canvas.getContext("2d");
+        drawBackground();
+        drawArc({ x: 400, y: 300 }, { x: 50, y: 75 });
+        drawDiamond({ x: 300, y: 300 });
+        // drawDiamonds();
+        drawEllipse({ x: 400, y: 300 });
+        drawBubbles({ x: 200, y: 200 });
+        drawCurve();
+        drawText();
+    }
+    function drawBackground() {
+        console.log("Background");
+        let gradient = crc2.createRadialGradient(400, 300, 50, 400, 300, 800);
+        gradient.addColorStop(0, "white");
+        gradient.addColorStop(0.2, "turquoise");
+        gradient.addColorStop(1, "blue");
+        crc2.fillStyle = gradient;
+        crc2.fillRect(0, 0, 800, 600);
+    }
+    function drawArc(_position, _size) {
+        console.log("Arc");
+        let r1 = 30;
+        let r2 = 150;
+        let gradient = crc2.createRadialGradient(0, 0, r1, 0, 0, r2);
+        gradient.addColorStop(0, "white");
+        gradient.addColorStop(0.5, "blue");
+        crc2.save();
+        crc2.translate(_position.x, _position.y);
+        crc2.fillStyle = gradient;
+        crc2.arc(0, 0, 50, 0, 2 * Math.PI);
+        crc2.fill();
+        crc2.restore();
+    }
+    function drawDiamond(_position) {
+        console.log("Diamond");
+        let sizeX = 40; // Breite des Diamanten
+        let sizeY = 60;
+        let color = "darkviolet";
+        crc2.save();
+        crc2.translate(_position.x, _position.y);
+        crc2.fillStyle = color;
+        crc2.beginPath();
+        crc2.moveTo(0, -sizeY / 2);
+        crc2.lineTo(sizeX / 2, 0);
+        crc2.lineTo(0, sizeY / 2);
+        crc2.lineTo(-sizeX / 2, 0);
+        crc2.closePath();
+        crc2.fill();
+        crc2.restore();
+    }
+    // function drawDiamonds(): void {
+    //     console.log("Drawing diamonds...");
+    //     let nDiamonds: number = 9;
+    //     let size: number = 30;
+    //     let startX: number = 50;
+    //     let startY: number = 50;
+    //     let spacingX: number = 60;
+    //     let spacingY: number = 60;
+    //     for (let i = 0; i < nDiamonds; i++) {
+    //         let posX = startX + i * spacingX;
+    //         for (let j = 0; j < nDiamonds; j++) {
+    //             let posY = startY + j * spacingY;
+    //             drawDiamond({x: posX, y: posY});
+    //         }
+    //     }
+    // }
+    function drawEllipse(_position) {
+        console.log("Ellipse");
+        let radiusX = 170;
+        let radiusY = 100;
+        let color = "violet";
+        crc2.save();
+        crc2.translate(_position.x, _position.y);
+        crc2.strokeStyle = color;
+        crc2.beginPath();
+        crc2.ellipse(0, 0, radiusX, radiusY, 0, 0, 2 * Math.PI);
+        crc2.fillStyle = "transparent";
+        crc2.fill();
+        crc2.stroke();
+        crc2.restore();
+    }
+    function drawBubbles(_position) {
+        console.log("Bubbles");
+        let r1 = 10;
+        let r2 = 20;
+        let gradient = crc2.createRadialGradient(0, 0, r1, 0, 0, r2);
+        gradient.addColorStop(0, "white");
+        gradient.addColorStop(0.5, "blue");
+        crc2.save();
+        crc2.translate(_position.x, _position.y);
+        crc2.fillStyle = gradient;
+        crc2.arc(0, 0, 20, 0, 2 * Math.PI);
+        crc2.fill();
+        crc2.restore();
+    }
+    function drawCurve() {
+        console.log("Curve");
+    }
+    function drawText() {
+        console.log("Text");
+    }
+})(GenerativeKunst || (GenerativeKunst = {}));
+//# sourceMappingURL=generativeKunst.js.map
---
+++ A_08/generativeKunst.ts
@@ -0,0 +1,141 @@
+namespace GenerativeKunst {
+
+    interface Vector {
+        x:number
+        y:number
+    }
+
+    window.addEventListener("load", handleLoad)
+    let crc2: CanvasRenderingContext2D;
+
+
+    function handleLoad(_event:Event):void{
+        let canvas: HTMLCanvasElement | null = document.querySelector("canvas");
+        if (!canvas)
+            return;
+        crc2= <CanvasRenderingContext2D>canvas.getContext("2d");
+
+        drawBackground();
+        drawArc({x:400, y:300}, {x:50,y:75});
+        drawDiamond({x:300, y:300});
+        // drawDiamonds();
+        drawEllipse({x:400, y:300});
+        drawBubbles({x:200, y:200});
+        drawCurve();
+        drawText();
+    }
+
+    function drawBackground():void {
+        console.log("Background");
+
+        let gradient:CanvasGradient = crc2.createRadialGradient(400, 300, 50, 400, 300, 800)
+        gradient.addColorStop(0, "white");
+        gradient.addColorStop(0.2, "turquoise")
+        gradient.addColorStop(1, "blue");      
+
+        crc2.fillStyle = gradient;
+        crc2.fillRect (0, 0, 800, 600);
+    }
+
+    function drawArc(_position:Vector, _size:Vector):void {
+        console.log("Arc");
+
+        let r1: number = 30;
+        let r2: number = 150;
+        let gradient: CanvasGradient = crc2. createRadialGradient(0, 0, r1, 0, 0, r2);
+
+        gradient.addColorStop(0, "white"); 
+        gradient.addColorStop(0.5, "blue");
+
+        crc2.save();
+        crc2.translate (_position.x, _position.y);
+        crc2.fillStyle = gradient;
+        crc2.arc(0, 0, 50, 0, 2 * Math.PI);
+        crc2.fill();
+        crc2.restore();
+    }
+
+    function drawDiamond(_position:Vector):void {
+        console.log("Diamond");
+
+        let sizeX: number = 40; // Breite des Diamanten
+        let sizeY: number = 60; 
+        let color: string = "darkviolet"; 
+
+        crc2.save();
+        crc2.translate(_position.x, _position.y);
+        crc2.fillStyle = color;
+        crc2.beginPath();
+        crc2.moveTo(0, -sizeY / 2); 
+        crc2.lineTo(sizeX / 2, 0);
+        crc2.lineTo(0, sizeY / 2); 
+        crc2.lineTo(-sizeX / 2, 0); 
+        crc2.closePath();
+        crc2.fill();
+        crc2.restore();
+    }
+
+    // function drawDiamonds(): void {
+    //     console.log("Drawing diamonds...");
+    //     let nDiamonds: number = 9;
+    //     let size: number = 30;
+    //     let startX: number = 50;
+    //     let startY: number = 50;
+    //     let spacingX: number = 60;
+    //     let spacingY: number = 60;
+    
+    //     for (let i = 0; i < nDiamonds; i++) {
+    //         let posX = startX + i * spacingX;
+    //         for (let j = 0; j < nDiamonds; j++) {
+    //             let posY = startY + j * spacingY;
+    //             drawDiamond({x: posX, y: posY});
+    //         }
+    //     }
+    // }
+
+    function drawEllipse(_position:Vector):void {
+        console.log("Ellipse");
+
+        let radiusX: number = 170; 
+        let radiusY: number = 100;
+        let color: string = "violet"; 
+    
+        crc2.save();
+        crc2.translate(_position.x, _position.y);
+        crc2.strokeStyle = color;
+        crc2.beginPath();
+        crc2.ellipse(0, 0, radiusX, radiusY, 0, 0, 2 * Math.PI);
+        crc2.fillStyle = "transparent"; 
+        crc2.fill(); 
+        crc2.stroke(); 
+        crc2.restore();
+    }
+
+    function drawBubbles(_position:Vector):void {
+        console.log("Bubbles");
+        let r1: number = 10;
+        let r2: number = 20;
+        let gradient: CanvasGradient = crc2. createRadialGradient(0, 0, r1, 0, 0, r2);
+
+        gradient.addColorStop(0, "white"); 
+        gradient.addColorStop(0.5, "blue");
+
+        crc2.save();
+        crc2.translate (_position.x, _position.y);
+        crc2.fillStyle = gradient;
+        crc2.arc(0, 0, 20, 0, 2 * Math.PI);
+        crc2.fill();
+        crc2.restore();
+
+    }
+
+
+    function drawCurve():void {
+        console.log("Curve");
+    }
+
+    function drawText():void {
+        console.log("Text");
+    }
+
+}
